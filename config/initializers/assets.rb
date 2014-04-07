61f734aec5f837da983254ac98ce632b79a796dd
Jonathan Robic <hello@jonathanrobic.fr>
2014-04-07 12:14:35 +0200

    essai dropdown menu locale

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,20 +1,21 @@
 Rails.application.config.assets.precompile << 'comfortable_mexican_sofa_metronic_admin/*.js'
 Rails.application.config.assets.precompile << 'comfortable_mexican_sofa_metronic_admin/*.css'
 
+
+
 Rails.application.config.assets.precompile << 'jquery/*.gif'
 Rails.application.config.assets.precompile << 'jquery/*.png'
-Rails.application.config.assets.precompile << 'jquery/*.gif'
 Rails.application.config.assets.precompile << 'jquery/*.jpg'
 
 Rails.application.config.assets.precompile << 'uniform/*.gif'
 Rails.application.config.assets.precompile << 'uniform/*.png'
-Rails.application.config.assets.precompile << 'uniform/*.gif'
 Rails.application.config.assets.precompile << 'uniform/*.jpg'
 
 Rails.application.config.assets.precompile << 'metronic/*.gif'
 Rails.application.config.assets.precompile << 'metronic/*.png'
-Rails.application.config.assets.precompile << 'metronic/*.gif'
 Rails.application.config.assets.precompile << 'metronic/*.jpg'
+
+Rails.application.config.assets.precompile << '/flags.png'
 
 Rails.application.config.assets.precompile << 'metronic/print.css'
 
